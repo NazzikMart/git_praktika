c00599192477e31d4a42c9e3eb8e1e6d86783259
Nazar <nazzikmart@gmail.com>
2023-07-19 20:20:43 +0300

    new comm

--- script.js
+++ script.js
@@ -1,88 +1,28 @@
-console.log(typeof undefined);
-console.log(typeof null);
+let user = {
+  name: "Nazar",
+  surname: "Martiniuk",
+  age: 20,
+  skills: ["JS", "HTML", "CSS"],
+  isMarried: false,
+  addAge: function () {
+    this.age = 21;
+  },
+};
+user.addAge();
 
-const firstName = "Nazar";
-const lastNmae = "Martiniuk";
-
-console.log(firstName, lastNmae);
-
-let age = 20;
-console.log(age);
-
-let x = 1;
-
-let b = ++x;
-console.log(b);
-
-console.log("Hello" + " world!");
-
-console.log(2 + Number("2"));
-
-if (5 === 5 && 2 === 3) {
-  console.log("false");
-} else if (5 === 5 && 2 === 2) {
-  console.log("true");
+for (let key in user) {
+  console.log(key, user[key]);
 }
 
-if (5 === 4 || 2 === 5) {
-  console.log("false");
-} else if (5 === 4 || 2 === 2) {
-  console.log("true");
+let numbers = [1, 2, 3, 4, 5];
+for (let key of numbers) {
+  console.log(key);
 }
 
-let salary = 2000;
+let str = "Nazzik Mart";
 
-if (salary > 3000) {
-  console.log(">3000");
-} else if (salary > 2000) {
-  console.log(" >2000");
-} else if (salary === 2000) {
-  console.log("2000");
-} else {
-  console.log("else...");
-}
-
-let status = "idle";
-
-switch (status) {
-  case "idle":
-    console.log("idle");
-    break;
-  case "idle":
-    console.log("idle");
-    break;
-  case "error":
-    console.log("error");
-    break;
-  default:
-    console.log("default");
-}
-
-const ages = 18;
-
-ages >= 18 ? console.log("18") : console.log("no 18");
-
-const number = [1, 2, 3, 4, 6, 7, 8];
-console.log(number);
-console.log(number.length);
-
-const strs = ["abs", "bsa"];
-
-strs.push("fafs");
-console.log(strs);
-
-for (let index = 0; index < strs.length; index++) {
-  const element = strs[index];
-  console.log(element.toString());
-}
-
-strs.map((item) => {
-  console.log(item.toString());
-});
-
-let i = prompt("Введіть своє ім'я:  ");
-if (i === "") {
-  alert("ви не ввели імені");
-} else {
-  alert("Доброго дня " + i);
-}
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.includes("Mart"));
+console.log(str.indexOf("Nazzik"));
+console.log(str.slice(0, 4));
