a72a3ca8b6ef69bbcfc3cf8d16933c7a3fc84490
Nazar <nazzikmart@gmail.com>
2023-07-24 08:14:01 +0300

    new comm 24/08/2023

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-let user = {
-  name: "Nazar",
-  surname: "Martiniuk",
-  age: 20,
-  skills: ["JS", "HTML", "CSS"],
-  isMarried: false,
-  addAge: function () {
-    this.age = 21;
-  },
+// function declaration
+
+function logs() {
+  console.log("declaration");
+}
+
+logs();
+
+// function expression
+const sum = function () {
+  console.log("expression");
 };
-user.addAge();
 
-for (let key in user) {
-  console.log(key, user[key]);
-}
+sum();
 
-let numbers = [1, 2, 3, 4, 5];
-for (let key of numbers) {
-  console.log(key);
-}
+// IIFE
+(function () {
+  console.log("IIFE");
+})();
 
-let str = "Nazzik Mart";
+// Arrow function
+const arrow = () => {
+  console.log("Arrow");
+};
 
-console.log(str.toLowerCase());
-console.log(str.toUpperCase());
-console.log(str.includes("Mart"));
-console.log(str.indexOf("Nazzik"));
-console.log(str.slice(0, 4));
+arrow();
+
+// Arrow function 2
+
+const arrow2 = () => console.log("Arrow2");
+arrow2();
