0aa2ba85bb131bbfc80201e1f77a9d4a7213ecc2
Nazar <nazzikmart@gmail.com>
2023-07-25 20:16:33 +0300

    new comm 25/07/2023

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
-// function declaration
+function twoSquared() {
+  return 2 * 2;
+}
+twoSquared();
 
-function logs() {
-  console.log("declaration");
+function copyArrayAndSquareNums(arr) {
+  const outpot = [];
+
+  for (let index = 0; index < arr.length; index++) {
+    outpot.push(arr[index] * arr[index]);
+  }
+
+  return outpot;
 }
 
-logs();
+let mass = [1, 2, 3, 4, 5];
 
-// function expression
-const sum = function () {
-  console.log("expression");
-};
-
-sum();
-
-// IIFE
-(function () {
-  console.log("IIFE");
-})();
-
-// Arrow function
-const arrow = () => {
-  console.log("Arrow");
-};
-
-arrow();
-
-// Arrow function 2
-
-const arrow2 = () => console.log("Arrow2");
-arrow2();
+let res = copyArrayAndSquareNums(mass);
+console.log(res);
