59036f5d9083e7bc67ac9ecadfd96bb5aadb9038
Nazar <nazzikmart@gmail.com>
2023-07-28 07:00:44 +0300

    new comm 28/07/2023

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-function twoSquared() {
-  return 2 * 2;
+function censor() {
+  const censoredArr = [];
+  return function (str1, str2 = "") {
+    if (str2) {
+      censoredArr.push([str1, str2]);
+    } else {
+      for (let pair of censoredArr) {
+        str1 = str1.replace(new RegExp(pair[0], "gi"), pair[1]);
+      }
+      return str1;
+    }
+  };
 }
-twoSquared();
 
-function copyArrayAndSquareNums(arr) {
-  const outpot = [];
-
-  for (let index = 0; index < arr.length; index++) {
-    outpot.push(arr[index] * arr[index]);
-  }
-
-  return outpot;
-}
-
-let mass = [1, 2, 3, 4, 5];
-
-let res = copyArrayAndSquareNums(mass);
-console.log(res);
+const changeScense = censor();
+changeScense("PHP", "JS");
+changeScense("backend", "frontend");
+console.log(
+  changeScense(
+    "PHP IS THE MOST POPULAR PROGRAMMING LANGUALE FOR backend WEB-DEVELOPMENT"
+  )
+);
